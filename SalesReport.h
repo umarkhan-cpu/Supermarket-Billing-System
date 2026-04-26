b996fa2b22851d301b614ccf5dc3cee90cf2474c
l253008-cmyk <l253008@lhr.nu.edu.pk>
2026-04-26 12:38:59 +0500

    removed billing and transaction classes.

--- SalesReport.h
+++ SalesReport.h
@@ -1,37 +1,13 @@
-#pragma once
+#ifndef SALESREPORT_H
+#define SALESREPORT_H
 #include <string>
 #include <fstream>
-using namespace std;
 
-class Transaction {
-private:
-    string date;
-    int receiptID;
-    double totalAmount;
+class SalesReport {
+    std::string filename; 
 
 public:
-    Transaction(string d = "", int id = 0, double amt = 0.0);
-
-    double getAmount() const;
-    string getDate() const;
-
-    bool readFromFile(ifstream& file);
-    void display() const;
+    SalesReport(std::string f);
+    void generatetotalsalesreport();
 };
-class BillingSystem {
-private:
-    string filename;
-
-public:
-    BillingSystem(string f);
-    void processSale(string date, int receiptID, double amount);
-};
-
-class ReportGenerator {
-private:
-    string filename;
-
-public:
-    ReportGenerator(string f);
-    void generateTotalSalesReport();
-};
+#endif
